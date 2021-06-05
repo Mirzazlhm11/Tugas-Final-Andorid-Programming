47a36859276f5dcaeaee79849ab2402b423616ba
ahmadfma <79648490+Mirzazlhm11@users.noreply.github.com>
2021-06-05 13:56:04 +0800

    buat class UserDatabase

---
+++ app/src/main/java/com/example/tugasfinal/User/UserDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.tugasfinal.User
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [PenumpangEntity::class], exportSchema = false, version = 1)
+abstract class UserDatabase: RoomDatabase() {
+
+    abstract fun penumpangDao(): PenumpangDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: UserDatabase? = null
+
+        fun getDatabase(context: Context): UserDatabase {
+            val tempinstance = INSTANCE
+            if(tempinstance != null) {
+                return tempinstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    UserDatabase::class.java,
+                    "user_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+
+}
