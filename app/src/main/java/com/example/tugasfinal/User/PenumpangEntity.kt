7d474912d538f9affb00f809d01a76bb3523c09c
ahmadfma <79648490+Mirzazlhm11@users.noreply.github.com>
2021-06-05 13:50:58 +0800

    buat class penumpangEntity

---
+++ app/src/main/java/com/example/tugasfinal/User/PenumpangEntity.kt
@@ -0,0 +1,18 @@
+package com.example.tugasfinal.User
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "penumpang_tabel")
+data class PenumpangEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    var nama: String,
+    var umur: String,
+    var alamat: String,
+    @ColumnInfo(name = "tempat_keberangkatan")
+    var tempatKeberangkatan: String,
+    @ColumnInfo(name = "tujuan_keberangkatan")
+    var tujuanKeberangkatan: String
+)
